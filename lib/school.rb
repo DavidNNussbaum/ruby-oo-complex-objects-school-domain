7cae24aa1150b6b6c4a70c6817264f3efcce8135
DavidNNussbaum <brocknynj@aol.com>
2020-11-08 10:57:26 -0500

    commit tree 60fb3239bbfaa8814ecfb199e441daadc041d4c8

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,41 @@
-# code here!
+require 'pry'
+class School 
+
+    attr_accessor :name, :roster    #:student, :grade
+    def initialize(name)
+        @name = name
+        @roster = {}
+    end
+
+    # def student(student)
+    #     @student = student
+    # end
+
+    def grade(stu_grade)
+        @stu_grade = stu_grade
+    end
+
+    def add_student(student, stu_grade)
+        if @roster.has_key?(stu_grade)
+           @roster[stu_grade].push(student)
+        #    binding.pry
+        else
+            @roster[stu_grade] = []
+            @roster[stu_grade].push(student)  
+        end  
+    end
+    def grade(stu_grade)
+        return @roster[stu_grade]
+    end
+
+    def sort
+        @roster.each do |k, v|
+            @roster[k] = v.sort!
+            # binding.pry
+            # grades.each do |students|
+            #     students.sort! 
+            # end
+        end
+        @roster
+    end
+end
